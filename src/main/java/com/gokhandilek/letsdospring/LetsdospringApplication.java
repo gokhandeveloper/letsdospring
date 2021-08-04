1c7dc1f393e1af03e7e7270b8864b9a3413b4a56
gokhandilek <gokhanengineering@gmail.com>
2021-08-04 19:26:49 +0800

    Initial commit

---
+++ src/main/java/com/gokhandilek/letsdospring/LetsdospringApplication.java
@@ -0,0 +1,13 @@
+package com.gokhandilek.letsdospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsdospringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsdospringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gokhandilek/letsdospring/LetsdospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokhandilek.letsdospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsdospringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
