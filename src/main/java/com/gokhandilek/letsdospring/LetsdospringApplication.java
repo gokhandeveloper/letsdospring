dcdfcd36d103b342b5db52646c41f49400b07d78
gokhandilek <gokhanengineering@gmail.com>
2021-08-04 21:00:41 +0800

    initial commit

--- src/main/java/com/gokhandilek/letsdospring/LetsdospringApplication.java
+++ src/main/java/com/gokhandilek/letsdospring/LetsdospringApplication.java
@@ -1,13 +1,32 @@
 package com.gokhandilek.letsdospring;
 
+import com.gokhandilek.letsdospring.data.entity.Room;
+import com.gokhandilek.letsdospring.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LetsdospringApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(LetsdospringApplication.class, args);
+    }
+
+    @RestController
+    public class  RoomController {
+        @Autowired
+
+        private RoomRepository roomRepository;
+
+        @GetMapping
+        public Iterable<Room> getRooms() {
+            return this.roomRepository.findAll();
+        }
+
     }
 
 }
